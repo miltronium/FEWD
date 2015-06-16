f39cd95879d60a4ecf8a35aaf9e8e046dc07b486
miltronium <milTronium@milTroniuns-MacBook-Pro.local>
2015-06-15 19:24:07 -0700

    uploaded slider code
    began js slider project

--- starter_code/js/app.js
+++ starter_code/js/app.js
@@ -4,6 +4,13 @@ function Thermostat(){
 
   /* declare variables needed for Thermostat here */
   var currentTemp = 64;
+  var upButton= document.getElementById('up');
+  var downButton= document.getElementById('down');
+  var temperatureText= document.getElementById('temp');
+  var body = document.getElementsByTagName('body')[0];
+
+  console.log(body);
+
 
   var convertTempToColor = function(temp){
 
@@ -14,13 +21,27 @@ function Thermostat(){
     return 'hsl('+hue+',70%,60%)';
 
   };
+var changeTemperature = function(temp){
+  temperatureText .innerHTML = temp + '<span>&deg;</span>'
+  body.style.backgroundColor = convertTempToColor(temp);
+};
 
   /* This function initializes the module */
   var init = function(){
 
     console.log('Thermostat is on.');
     console.log('The current temperature is '+currentTemp+'.');
+    changeTemperature(currentTemp);
 
+    upButton.addEventListener('mousedown',function(){
+      currentTemp = currentTemp + 1;
+      changeTemperature(currentTemp);
+})
+      downButton.addEventListener('mousedown',function(){
+        currentTemp = currentTemp - 1;
+        changeTemperature(currentTemp);
+
+    });
   };
 
   /* This line calls init() function. */
